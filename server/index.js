9223f6edabd365cc507921677eed6a431efd664f
Zach Locker <zacharywlocker@gmail.com>
2025-06-12 15:14:11 -0400

    Test get song logiv

--- server/index.js
+++ server/index.js
@@ -1,6 +1,24 @@
 import dotenv from "dotenv";
+import { getData, getURI } from "./services/songFetch.js";
+import { getAccessToken } from "./services/spotifyAuth.js";
 import { connectDB } from "./config/db.js";
 
 dotenv.config();
 
-connectDB();
+async function main() {
+  connectDB();
+
+  const d = new Date();
+  let day = d.getDay();
+
+  const id = process.env.CLIENT_ID;
+  const secret = process.env.CLIENT_SECRET;
+
+  const token = await getAccessToken(id, secret);
+  const uri = await getURI(day);
+
+  await getData(uri, token, day);
+  console.log("Song added successfully.");
+}
+
+main();
