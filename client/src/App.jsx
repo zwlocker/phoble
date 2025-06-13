1fcd3d33393925f3db6819fe52a2c2b705bdbf8c
Zach Locker <zacharywlocker@gmail.com>
2025-06-12 23:54:26 -0400

    Install Tailwind

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  let [count, setCount] = useState(0);
 
+  function handleClick() {
+    setCount(count + 1);
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <button
+      onClick={handleClick}
+      className="bg-blue-500 text-white cursor-pointer m-2 rounded-md px-3 py-1 text-sm shadow-lg shadow-neutral-500/20 transition active:scale-[.95]"
+    >
+      {count}
+    </button>
+  );
+};
 
-export default App
+export default App;
