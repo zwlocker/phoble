97bdef8e2b858bb81a20c0b7fb2b9ef0785feb99
Zach Locker <zacharywlocker@gmail.com>
2025-08-26 11:40:42 -0400

    Set origin to true for testing

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ async function main() {
 
   app.use(
     cors({
-      origin: "https://www.phoble.net",
+      origin: true,
       credentials: true,
       methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
       allowedHeaders: ["Content-Type", "Authorization"],
