399388349f040ebbd88c15debb5873c890b977c6
Zach Locker <zacharywlocker@gmail.com>
2025-09-08 15:40:10 -0400

    Revert changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,38 +6,34 @@ import { AuthProvider } from "./contexts/AuthContext";
 import Init from "./components/Init/Init";
 import { ToastContainer, Zoom } from "react-toastify";
 import Profile from "./components/Profile/Profile";
-import { Analytics } from "@vercel/analytics/next";
 
 const App = () => {
   return (
-    <>
-      <Router>
-        <AuthProvider>
-          <Routes>
-            <Route path="/" element={<SongPage />} />
-            <Route path="/song/:id" element={<SongPage />} />
-            <Route path="/history" element={<SongGrid />} />
-            <Route path="/initUser" element={<Init />} />
-            <Route path="/profile" element={<Profile />} />
-          </Routes>
-          <ToastContainer
-            position="bottom-center"
-            autoClose={2500}
-            hideProgressBar={true}
-            newestOnTop={false}
-            closeOnClick={true}
-            rtl={false}
-            pauseOnFocusLoss
-            draggable
-            pauseOnHover
-            theme
-            transition={Zoom}
-            limit={3}
-          />
-        </AuthProvider>
-      </Router>
-      <Analytics />
-    </>
+    <Router>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<SongPage />} />
+          <Route path="/song/:id" element={<SongPage />} />
+          <Route path="/history" element={<SongGrid />} />
+          <Route path="/initUser" element={<Init />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+        <ToastContainer
+          position="bottom-center"
+          autoClose={2500}
+          hideProgressBar={true}
+          newestOnTop={false}
+          closeOnClick={true}
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme
+          transition={Zoom}
+          limit={3}
+        />
+      </AuthProvider>
+    </Router>
   );
 };
 
